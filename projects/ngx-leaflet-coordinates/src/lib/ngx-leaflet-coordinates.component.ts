acfa0a7826f6e481c7e6bab0a1f29544dcf01b21
friont <thomas.frion@etu.univ-grenoble-alpes.fr>
2020-07-13 11:49:48 +0200

    [WIP] Built but ignore my types def

--- projects/ngx-leaflet-coordinates/src/lib/ngx-leaflet-coordinates.component.ts
+++ projects/ngx-leaflet-coordinates/src/lib/ngx-leaflet-coordinates.component.ts
@@ -1,13 +1,60 @@
+
 import { Component, OnInit, OnDestroy, Input } from '@angular/core';
 import * as L from 'leaflet';
 
-import 'node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.min.js'
+import '../../../../node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.min.js'
+
+declare module 'leaflet' {
+  namespace Control {
+      interface CoordinatesOptions extends L.ControlOptions {
+          decimals: number,
+          decimalsSeperator: string,
+          labelTemplateLat?: string,
+          labelTemplateLng?: string,
+          labelFormatterLat?: any, // FCT
+          labelFormatterLng?: any, // FCT
+          enableUserInput?: boolean,
+          useDMS?: boolean,
+          useLatLngOrder?: boolean,
+          centerUserCoordinates?: boolean,
+          markerType?: L.Marker,
+          markerProps?: L.MapOptions
+      }
+// 
+      export class Coordinates extends L.Control {
+          constructor(otpions?: CoordinatesOptions);
+          // 
+          _createInput(classname: string, container: HTMLElement) : HTMLElement;
+          _clearMarker() : void;
+          _handleKeypress(e : any) : void;
+          _handleSubmit() : void;
+          expand() : void;
+          _createCoordinateLabel(ll : any) : string;
+          _getNumber(n : number, opts : CoordinatesOptions): string;
+          collapse() : void;
+          _switchUI(evt : any): void;
+          _update(evt : any) : void;
+          _createNewMarker() : L.Marker;
+      }
+  }
+// 
+  namespace control {
+      export function coordinates(options?: Control.CoordinatesOptions) : Control.Coordinates;
+  }
+// 
+  export namespace NumberFormatter {
+      export function round(num : number, dec : number, sep : string) : string;
+      export function toDMS(deg : number) : string;
+      export function createValidNumber(num: string, sep : string) : number | undefined;
+  }
+}
+
 
 @Component({
   selector: 'leaflet-coordinates-control',
   template: '',
   styleUrls: [
-    '../../../../node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.min.js'
+    '../../../../node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.css'
   ]
 })
 export class NgxLeafletCoordinatesComponent implements OnInit, OnDestroy {
--- projects/ngx-leaflet-coordinates/src/public-api.ts
+++ projects/ngx-leaflet-coordinates/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of ngx-leaflet-coordinates
  */
 
-export * from './lib/ngx-leaflet-coordinates.service';
 export * from './lib/ngx-leaflet-coordinates.component';
 export * from './lib/ngx-leaflet-coordinates.module';
