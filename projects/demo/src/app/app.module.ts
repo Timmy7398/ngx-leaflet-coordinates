50e5a7d0496f1e31b5ba3f9691d8a8008c534caa
friont <thomas.frion@etu.univ-grenoble-alpes.fr>
2020-07-13 14:31:11 +0200

    [WIP] The lib works

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { LeafletModule } from '@asymmetrik/ngx-leaflet';
+import { NgxLeafletCoordinatesModule } from '@timmy73/ngx-leaflet-coordinates';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    LeafletModule
+    LeafletModule,
+    NgxLeafletCoordinatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
