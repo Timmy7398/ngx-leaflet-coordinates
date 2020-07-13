843b0bf1d0e7f539a022d164d0db00342bc5c77e
friont <thomas.frion@etu.univ-grenoble-alpes.fr>
2020-07-13 14:17:55 +0200

    [WIP] Add a app for test

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import * as L from 'leaflet';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  
+  title = 'demo';
+  map : L.Map;
+
+  options = {
+    layers: [
+      L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
+    ],
+    zoom: 5,
+    center: L.latLng(46.879966, -121.726909)
+  };
+
+  baseLayers = {}
+
+  ngOnInit(): void {
+  }
+
+  onMapReady(map : L.Map){
+    this.map = map;
+  }
+}
