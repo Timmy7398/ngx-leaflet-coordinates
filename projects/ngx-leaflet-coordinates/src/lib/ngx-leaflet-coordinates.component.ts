bfd528fafeae05ca2bfd9936e977f617d61b7354
friont <thomas.frion@etu.univ-grenoble-alpes.fr>
2020-07-13 11:20:26 +0200

    [WIP] Definition of the component

--- projects/ngx-leaflet-coordinates/src/lib/ngx-leaflet-coordinates.component.ts
+++ projects/ngx-leaflet-coordinates/src/lib/ngx-leaflet-coordinates.component.ts
@@ -1,20 +1,44 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy, Input } from '@angular/core';
+import * as L from 'leaflet';
+
+import 'node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.min.js'
 
 @Component({
-  selector: 'lib-ngx-leaflet-coordinates',
-  template: `
-    <p>
-      ngx-leaflet-coordinates works!
-    </p>
-  `,
-  styles: [
+  selector: 'leaflet-coordinates-control',
+  template: '',
+  styleUrls: [
+    '../../../../node_modules/leaflet.coordinates/dist/Leaflet.Coordinates-0.1.5.min.js'
   ]
 })
-export class NgxLeafletCoordinatesComponent implements OnInit {
+export class NgxLeafletCoordinatesComponent implements OnInit, OnDestroy {
+
+  private _map : L.Map;
+  private coordinates : L.Control.Coordinates;
 
   constructor() { }
 
   ngOnInit(): void {
   }
+
+  ngOnDestroy(): void {
+    this._map.removeControl(this.coordinates);
+  }
+
+  @Input() options : L.Control.CoordinatesOptions = {
+    decimals: 4,
+    decimalsSeperator: '.'
+  };
+
+  @Input() set map(map : L.Map) {
+    if(map){
+      this._map = map;
+      this.coordinates = L.control.coordinates(this.options).addTo(this.map);
+    }
+  }
+
+  get map() : L.Map {
+    return this._map;
+  }
+
 
 }
