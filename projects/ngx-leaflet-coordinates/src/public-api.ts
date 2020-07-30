032a9feb9a1f52157bd460049dbcc2ba9c932ff9
friont <frion.thomas@gmail.com>
2020-07-30 13:44:28 +0200

    Backup

--- projects/ngx-leaflet-coordinates/src/public-api.ts
+++ projects/ngx-leaflet-coordinates/src/public-api.ts
@@ -1,9 +1,7 @@
-// <reference path='./index.ts'/>
 /*
  * Public API Surface of ngx-leaflet-coordinates
  */
 
-export * from './index';
-export * from './lib/NumberFormatter';
+export * from './lib/ngx-leaflet-coordinates.service';
 export * from './lib/ngx-leaflet-coordinates.component';
 export * from './lib/ngx-leaflet-coordinates.module';
